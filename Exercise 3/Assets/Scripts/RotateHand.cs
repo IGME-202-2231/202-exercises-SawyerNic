967c9e91a092c1d454af5fdf75dfcbebe88c40c3
Sawyer Nicastro <sjn1618@g.rit.edu>
2023-09-12 16:45:22 -0400

    exercise 3 progress

---
+++ Exercise 3/Assets/Scripts/RotateHand.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float turnAmount = 0;
+    bool useDeltaTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sec = 100 * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, 0, 45+sec);
+    }
+}
