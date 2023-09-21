9e01525b86176acb222ff401ebeb40f54e4a280d
Sawyer Nicastro <sjn1618@g.rit.edu>
2023-09-21 16:44:11 -0400

    car exercise

--- Exercise 4/Assets/Scripts/Vehicle.cs
+++ Exercise 4/Assets/Scripts/Vehicle.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Vehicle : MonoBehaviour
 {
-    Vector3 vehiclePosition = new Vector3(0,0,0);
+    Vector3 vehiclePosition = new Vector3(0, 0, 0);
 
+    [SerializeField]
     float speed = 4f;
 
-    Vector3 direction = new Vector3(1,0,0);
+    Vector3 direction = Vector3.zero;
 
-    Vector3 velocity = new Vector3(0,0,0);
+    Vector3 velocity = Vector3.zero;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +29,28 @@ public class Vehicle : MonoBehaviour
 
         vehiclePosition += velocity;
 
+        //Leaving space for movement validation
+
+        if (vehiclePosition.y > 5 || vehiclePosition.y < -5)
+        {
+            vehiclePosition.y = -vehiclePosition.y;
+
+        }
+        else if(vehiclePosition.x > 5 || vehiclePosition.x < -5)
+        {
+            vehiclePosition.x = -vehiclePosition.x;
+        }
         transform.position = vehiclePosition;
+
+    }
+
+    public void SetDirection(Vector3 newDirection)
+    {
+        direction = newDirection.normalized;
+
+        if(direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        }
     }
 }
--- Exercise 4/Assets/Scripts/VehicleController.cs
+++ Exercise 4/Assets/Scripts/VehicleController.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VehicleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Vehicle m_VehicleController;
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        m_VehicleController.SetDirection(context.ReadValue<Vector2>()); 
     }
 }
